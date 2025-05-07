63ac329be582668d01a3baa5c1d541f84cf3e819
Omar Camarillo <116605975+omarcamarillo@users.noreply.github.com>
2025-05-06 22:33:57 -0700

    Add files via upload

---
+++ main.js
@@ -0,0 +1,9 @@
+const btnMenu = document.getElementById("btn__menu")
+const menu = document.getElementById("menu__content")
+
+btnMenu.addEventListener("click", ()=> {
+    menu.classList.toggle("hidden__menu")
+})
+
+
+//Post demo
